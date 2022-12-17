0836f874adf500479c9238663578e04435749633
Jazeb (SE) <sheikhalishba878@gmail.com>
2022-12-17 18:33:55 +0500

    coding files

---
+++ car.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout << "             * * * * * * * * * * * *                                " << endl;
+cout << "           ***************************         " << endl;
+cout << "         *    **********************   *        " << endl;
+cout << "       *    *     | | | | | | | |   *    *       " << endl;
+cout << "     *   *        | | | | | | | |     *    *     " << endl;
+cout << "************************************************   " << endl;
+cout << "*                                              *" << endl;
+cout << "*                   C A R                      *  " << endl;
+cout << "*                                              *                            " << endl;
+cout << "*        *  *                         *  *     *             " << endl;
+cout << "*      *      *                     *      *   *                  " << endl;
+cout << "****** *      * ******************* *      * ***                          *  " << endl;
+cout << "         *  *                         *  *      " << endl;
+cout << "                                                " << endl;
+cout << "                                                " << endl;
+cout << "                                                " << endl;
+cout << "                                                " << endl;
+cout << "                                                " << endl;
+}c
---
+++ creativity.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+main()
+{                                                             
+cout << "              __         __                   " << endl;
+cout << "             /  1       /  1                 " << endl;
+cout << "            /    1     /    1               " << endl;
+cout << "            1  ^  * * *  ^  1                 " << endl;
+cout << "             1 *  ^   ^   *1                 " << endl;
+cout << "             * ---  o  ---  *               " << endl;
+cout << "            *      ---       *             " << endl;
+cout << "            *                *             " << endl;
+cout << "             ****************                " << endl;
+cout << "              /     o     1              " << endl;
+cout << "              1  _     _  1              " << endl;
+cout << "             / 1__1   1__1 1                " << endl;
+cout << "            /               1            " << endl;
+cout << "           / ____       ____ 1             " << endl;
+cout << "          / /    1     /    1 1         " << endl;
+cout << "          1 1___ 1    /  ___/ /         " << endl;
+cout << "          1______/   1_______/                " << endl;
+}
